0527638ee7a50b1398adaeed17f0e04220273c0d
Wali Morris <walimmorris@gmail.com>
2023-04-20 16:20:17 -0700

    add jQuery to write the employee slide component animations

--- src/main/resources/static/js/employee-slide-component.js
+++ src/main/resources/static/js/employee-slide-component.js
@@ -1,5 +1,45 @@
-const toggle = document.getElementById('toggle');
-addEventListener('click', (e) => {
-    document.querySelector('#slideSet').classList.toggle('paused');
-    console.log('clicked')
+var slide = 1;
+
+$(document).ready(function () {
+    $('#dot-1').css('background-color', '#743089');
+    setInterval(slideshow, 4000);
 });
+
+function slideshow() {
+    if (slide === 3) {
+        slide = 1;
+    } else {
+        slide = slide + 1;
+    }
+    switch (slide) {
+        case 1:
+            $('#slide-1').css('visibility', 'visible');
+            $('#slide-2').css('visibility', 'hidden');
+            $('#slide-3').css('visibility', 'hidden');
+
+            // update respective dot
+            $('#dot-1').css('background-color', '#743089');
+            $('#dot-2').css('background-color', '#ddd');
+            $('#dot-3').css('background-color', '#ddd');
+            break;
+        case 2:
+            $('#slide-1').css('visibility', 'hidden');
+            $('#slide-2').css('visibility', 'visible');
+            $('#slide-3').css('visibility', 'hidden');
+
+            // update respective dot
+            $('#dot-1').css('background-color', '#ddd');
+            $('#dot-2').css('background-color', '#743089');
+            $('#dot-3').css('background-color', '#ddd');
+            break;
+        default:
+            $('#slide-1').css('visibility', 'hidden');
+            $('#slide-2').css('visibility', 'hidden');
+            $('#slide-3').css('visibility', 'visible');
+
+            // update respective dot
+            $('#dot-1').css('background-color', '#ddd');
+            $('#dot-2').css('background-color', '#ddd');
+            $('#dot-3').css('background-color', '#743089');
+    }
+}
--- src/main/resources/static/js/pagination-actions.js
+++ src/main/resources/static/js/pagination-actions.js
@@ -1,25 +1,26 @@
-const url = window.location.href;
-const params = url.split('?')[1];
+$(document).ready(function () {
+    activatePaginationLink();
+})
 
-if (!params.includes('page')) {
-    const link = document.getElementById(`link-0`);
-    link.style.backgroundColor = '#ddd';
-} else {
-    const digit1 = parseInt(params.split('page')[1].charAt(1));
-    let digit2 = -1;
-    if (digit1 >= 1 && checkDigit(params.split('page')[1].charAt(2))) {
-        digit2 = parseInt(params.split('page')[1].charAt(2));
-    }
+function activatePaginationLink() {
+    const url = window.location.href;
+    const params = url.split('?')[1];
 
-    if (digit2 > -1) {
-        const page = `${digit1.toString()}${digit2.toString()}`;
-        console.log(page);
-        const pageNumber = parseInt(page);
-        const link = document.getElementById(`link-${pageNumber.toString()}`);
-        link.style.backgroundColor = '#ddd';
+    if (!params.includes('page')) {
+        $('#link-0').css('background-color', '#ddd');
     } else {
-        const link = document.getElementById(`link-${digit1.toString()}`);
-        link.style.backgroundColor = '#ddd';
+        const digit1 = parseInt(params.split('page')[1].charAt(1));
+        let digit2 = -1;
+        if (digit1 >= 1 && checkDigit(params.split('page')[1].charAt(2))) {
+            digit2 = parseInt(params.split('page')[1].charAt(2));
+        }
+        if (digit2 > -1) {
+            const page = `${digit1.toString()}${digit2.toString()}`;
+            const pageNumber = parseInt(page);
+            $(`#link-${pageNumber.toString()}`).css('background-color', '#ddd');
+        } else {
+            $(`#link-${digit1.toString()}`).css('background-color', '#ddd');
+        }
     }
 }
 

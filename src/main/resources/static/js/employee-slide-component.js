6ae0d90de7a9633d926089d9abd3506e06719d55
Wali Morris <walimmorris@gmail.com>
2023-04-20 19:45:53 -0700

    style employee slide and add functionality

--- src/main/resources/static/js/employee-slide-component.js
+++ src/main/resources/static/js/employee-slide-component.js
@@ -1,17 +1,42 @@
 var slide = 1;
 
 $(document).ready(function () {
+    // on ready, begin with slide 1
     $('#dot-1').css('background-color', '#743089');
     setInterval(slideshow, 4000);
+
+    // event listeners on slide dots
+    $('#dot-1').on('click', function () {updateSlide(1);}); // user clicks slide 1 dot
+    $('#dot-2').on('click', function () {updateSlide(2);}); // user clicks slide 2 dot
+    $('#dot-3').on('click', function () {updateSlide(3);}); // user clicks slide 3 dot
+
+    // event listeners on next & prev buttons
+    $('#prev-button').on('click', function () {updateSlide(slide - 1);}); // user clicks prev, subtract 1
+    $('#next-button').on('click', function () {updateSlide(slide + 1);}); // user clicks next, add 1
 });
 
+/**
+ * Used in conjunction with setInterval to create a slideshow animation
+ * functionality on the employee-slide component. Updates a global var
+ * 'slide' to iterate through elements.
+ */
 function slideshow() {
-    if (slide === 3) {
+    if (slide >= 3) {
         slide = 1;
     } else {
         slide = slide + 1;
     }
-    switch (slide) {
+    updateSlide(slide);
+}
+
+/**
+ * Updates the slide element's and respective slide dot element's visibility
+ * attribute based on the supplied slide
+ * @param n
+ */
+function updateSlide(n) {
+    slide = n;
+    switch (n) {
         case 1:
             $('#slide-1').css('visibility', 'visible');
             $('#slide-2').css('visibility', 'hidden');
